71cacf7246af760f779e9e81226ce4ff29939992
Joe Danks <joedanks@gmail.com>
2024-12-03 21:27:28 -0600

    Initial spring boot starter

---
+++ src/main/java/com/joedanks/adventofcode/AdventOfCode2024Application.java
@@ -0,0 +1,13 @@
+package com.joedanks.adventofcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCode2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventOfCode2024Application.class, args);
+	}
+
+}
